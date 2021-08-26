7f3c3227bd9688e227dff479126ff6f08db665a5
Ivan-Albornoz <89415907+Ivan-Albornoz@users.noreply.github.com>
2021-08-26 16:24:45 -0400

    Initial commit

---
+++ StringCalculatorBlank/StringCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorBlank
+{
+    public class StringCalculator
+    {
+
+        public int Add(string numbers)
+        {
+            return numbers == "" ? 0 : numbers.Split(',')
+                .Select(int.Parse)
+                .Sum();
+        }
+    }
+}
+
